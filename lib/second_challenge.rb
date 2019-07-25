d930591ce1d829308550054388e19712cd441146
Eliza Munroe <era3munroe@gmail.com>
2019-07-25 22:19:05 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,15 +2,15 @@ def second_challenge
   epic_tragedy = {
     :montague => {
        patriarch: {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends => []
+        matriarch: {}
+        hero: {}
+        hero_friends: []
       }, 
     :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
-      :heroine_friends => []
+        patriarch: {}
+        matriarch: {}
+        heroine: {}
+        heroine_friends: []
       #your key/value pairs here!
       }}
 
